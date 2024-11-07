c265fdb854a3a2f74bf9eb1227902824070b6b23
Benjamin Cutler <benjamin.cutler@datarobot.com>
2024-11-07 16:59:46 -0500

    Don't need th `/doom` URL

--- app.py
+++ app.py
@@ -10,18 +10,12 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1)
 
-
 @app.route("/")
-def hello_route():
-    return {"hello": "world"}
-
-
-@app.route("/doom/")
 def index_route():
     return send_from_directory('.', 'index.html')
 
 
-@app.route('/doom/<path:filename>')
+@app.route('/<path:filename>')
 def serve_static_file(filename):
     return send_from_directory('.', filename)
 if __name__ == "__main__":
